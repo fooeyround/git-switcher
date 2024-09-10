7c57855a45fe1b230551cb9f65a4af9c77555295
fooeyround <JackTheDev@gmail.com>
2024-09-10 03:46:43 +0000

    update package, if not, this needs to be put in a normal nixpkg

--- flake.nix
+++ flake.nix
@@ -1,21 +1,30 @@
-git-switcher = buildGoModule rec {
-  pname = "pet";
+{ lib, buildGoModule, fetchFromGitHub}: 
+buildGoModule rec {
+  pname = "git-switcher";
   version = "0.3.0";
 
 #  src = fetchFromGitHub {
-#    owner = "knqyf263";
-#    repo = "gi";
+#    owner = "TheYkk";
+#    repo = "git-switcher";
 #    rev = "v${version}";
 #    hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
 #  };
+
   src = "./.";
-  vendorSha256 = "sha256-ciBIR+a1oaYH+H1PcC8cD8ncfJczk1IiJ8iYNM+R6aA=";
   vendorHash = "sha256-ciBIR+a1oaYH+H1PcC8cD8ncfJczk1IiJ8iYNM+R6aA=";
 
-  meta = with lib; {
+  CGO_ENABLED = 0;
+
+  outputs = [ "out" "man" ];
+
+  ldflags = [
+    "-s" "-w" "-X main.version=${version} -X main.revision=${version}"
+  ];
+
+  meta = {
     description = "Easily switch between your git profiles";
     homepage = "https://github.com/TheYkk/git-switcher";
-    license = licenses.apache;
-    maintainers = with maintainers; [ fooeyround ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ zowoq ]; #TEMP: should be `fooeyround` but that is not in the maintainer list
   };
 }
