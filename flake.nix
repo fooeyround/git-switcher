80ec73cf0f4f0da18bcb67522f158bcdfe52e7fe
fooeyround <JackTheDev@gmail.com>
2024-09-10 04:21:06 +0000

    make flake.nix actually a flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,38 @@
-{ lib, buildGoModule, fetchFromGitHub}: 
-buildGoModule rec {
-  pname = "git-switcher";
-  version = "0.3.0";
+{
+  description = "A simple Nix flake";
 
-  src = fetchFromGitHub {
-    owner = "TheYkk";
-    repo = "git-switcher";
-    rev = "v${version}";
-    hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
-  };
+  outputs = { self, nixpkgs, ... }@inputs: {
+    packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; buildGoModule rec {
+      pname = "git-switcher";
+      version = "0.3.0";
 
-  #src = "./.";
-  vendorHash = null;
+    src = fetchFromGitHub {
+      owner = "TheYkk";
+      repo = "git-switcher";
+      rev = "v${version}";
+      hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
+    };
 
-  CGO_ENABLED = 0;
+    vendorSha256 = null;
+    #src = "./.";
+    vendorHash = null;
 
-  outputs = [ "out" "man" ];
+    CGO_ENABLED = 0;
 
-  ldflags = [
-    "-s" "-w" "-X main.version=${version} -X main.revision=${version}"
-  ];
+    outputs = [ "out" "man" ];
 
-  meta = {
-    description = "Easily switch between your git profiles";
-    homepage = "https://github.com/TheYkk/git-switcher";
-    license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ zowoq ]; #TEMP: should be `fooeyround` but that is not in the maintainer list
+    ldflags = [
+      "-s" "-w" "-X main.version=${version} -X main.revision=${version}"
+    ];
+
+
+
+    buildInputs = [ ];
+      meta = {
+        description = "Easily switch between your git profiles";
+        license = lib.licenses.asl20;
+        maintainers = with lib.maintainers; [ ];
+      };
+    };
   };
 }
